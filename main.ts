0b26fa7ee4a222ea831b95a14b635747cfc16e1b
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2021-05-24 20:09:07 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
     music.playMelody("B G B G B G B G ", 120)
     basic.showLeds(`
         . . . . .
@@ -9,16 +9,14 @@ radio.onReceivedNumber(function (receivedNumber) {
         . . . . .
         . . . . .
         `)
-    basic.pause(10000)
-    basic.clearScreen()
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    blinken()
 })
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P1, 1)
     radio.sendNumber(1)
-    music.playMelody("B G B G B G B G ", 120)
+    blinken()
     basic.showIcon(IconNames.Yes)
+    music.playMelody("B G B G B G B G ", 120)
     basic.pause(1000)
     basic.clearScreen()
     basic.pause(10000)
@@ -39,4 +37,13 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P1, 0)
 })
-music.setBuiltInSpeakerEnabled(true)
+function blinken () {
+    for (let index = 0; index < 10; index++) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(200)
+    }
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+radio.setGroup(1)
